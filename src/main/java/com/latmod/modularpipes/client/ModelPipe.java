da1707120a8c1448560281cb51023bf5f4442849
LatvianModder <latvianmodder@gmail.com>
2019-01-12 13:59:08 +0200

    Split ModelPipe into multiple classes

--- src/main/java/com/latmod/modularpipes/client/ModelPipe.java
+++ src/main/java/com/latmod/modularpipes/client/ModelPipe.java
@@ -3,44 +3,26 @@ package com.latmod.modularpipes.client;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.latmod.modularpipes.ModularPipes;
-import com.latmod.modularpipes.block.BlockPipeBase;
-import com.latmod.modularpipes.block.BlockPipeModular;
 import com.latmod.modularpipes.block.EnumMK;
 import com.latmod.modularpipes.block.PipeSkin;
-import com.latmod.modularpipes.tile.TilePipeBase;
-import com.latmod.modularpipes.tile.TilePipeModularMK1;
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
-import net.minecraft.block.Block;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.block.model.ItemOverrideList;
 import net.minecraft.client.renderer.block.model.ModelRotation;
 import net.minecraft.client.renderer.block.statemap.DefaultStateMapper;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.VertexFormat;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
-import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.client.model.PerspectiveMapWrapper;
 import net.minecraftforge.common.model.IModelState;
 import net.minecraftforge.common.model.TRSRTransformation;
-import net.minecraftforge.common.property.IExtendedBlockState;
 
-import javax.annotation.Nullable;
 import java.util.AbstractMap;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -140,7 +122,7 @@ public class ModelPipe extends DefaultStateMapper implements IModel
 	@Override
 	public IBakedModel bake(IModelState state, VertexFormat format, Function<ResourceLocation, TextureAtlasSprite> tex)
 	{
-		return new Baked(this, tex.apply(textureParticle), (id, rotation, retextures) ->
+		return new ModelPipeBaked(this, tex.apply(textureParticle), (id, rotation, retextures) ->
 		{
 			ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
 
@@ -155,301 +137,8 @@ public class ModelPipe extends DefaultStateMapper implements IModel
 		});
 	}
 
-	private interface ModelCallback
+	public interface ModelCallback
 	{
 		List<BakedQuad> get(ResourceLocation id, ModelRotation rotation, Map.Entry... retextures);
-	}
-
-	private static class Baked implements IBakedModel
-	{
-		private final TextureAtlasSprite particle;
-		private final Map<PipeSkin, List<List<BakedQuad>>> base, connection;
-		private final List<List<BakedQuad>> glassBase, glassConnection, overlay;
-		private final Map<PipeSkin, Int2ObjectOpenHashMap<List<BakedQuad>>> cache;
-		private final IBakedModel bakedItem;
-		private final IBakedModel[] bakedItemWithOverlay;
-		private final ItemOverrideList itemOverrideList;
-
-		private final class BakedItem implements IBakedModel
-		{
-			private List<BakedQuad> quads;
-
-			public BakedItem(@Nullable EnumMK mk)
-			{
-				quads = new ArrayList<>();
-				quads.addAll(base.get(PipeSkin.NONE).get(0));
-				quads.addAll(glassBase.get(0));
-
-				if (mk != null)
-				{
-					quads.addAll(overlay.get(mk.ordinal()));
-				}
-
-				quads = Collections.unmodifiableList(Arrays.asList(quads.toArray(new BakedQuad[0])));
-			}
-
-			@Override
-			public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand)
-			{
-				return quads;
-			}
-
-			@Override
-			public boolean isAmbientOcclusion()
-			{
-				return true;
-			}
-
-			@Override
-			public boolean isGui3d()
-			{
-				return true;
-			}
-
-			@Override
-			public boolean isBuiltInRenderer()
-			{
-				return false;
-			}
-
-			@Override
-			public TextureAtlasSprite getParticleTexture()
-			{
-				return particle;
-			}
-
-			@Override
-			public ItemOverrideList getOverrides()
-			{
-				return ItemOverrideList.NONE;
-			}
-
-			@Override
-			public org.apache.commons.lang3.tuple.Pair<? extends IBakedModel, javax.vecmath.Matrix4f> handlePerspective(ItemCameraTransforms.TransformType cameraTransformType)
-			{
-				return handleModelPerspective(this, cameraTransformType);
-			}
-		}
-
-		public Baked(ModelPipe m, TextureAtlasSprite p, ModelCallback c)
-		{
-			particle = p;
-			base = new HashMap<>(PipeSkin.MAP.size());
-
-			for (PipeSkin skin : PipeSkin.MAP.values())
-			{
-				AbstractMap.SimpleEntry<String, ResourceLocation> entry = new AbstractMap.SimpleEntry<>("material", skin.texture);
-				List<List<BakedQuad>> base1 = new ArrayList<>(4);
-				base1.add(c.get(m.modelBase, ModelRotation.X0_Y0, entry));
-				base1.add(c.get(m.modelVertical, ModelRotation.X90_Y90, entry));
-				base1.add(c.get(m.modelVertical, ModelRotation.X0_Y0, entry));
-				base1.add(c.get(m.modelVertical, ModelRotation.X90_Y0, entry));
-				base.put(skin, base1);
-			}
-
-			glassBase = new ArrayList<>(4);
-			glassBase.add(c.get(m.modelGlassBase, ModelRotation.X0_Y0));
-			glassBase.add(c.get(m.modelGlassVertical, ModelRotation.X90_Y90));
-			glassBase.add(c.get(m.modelGlassVertical, ModelRotation.X0_Y0));
-			glassBase.add(c.get(m.modelGlassVertical, ModelRotation.X90_Y0));
-
-			connection = new HashMap<>(PipeSkin.MAP.size());
-			glassConnection = new ArrayList<>(6);
-
-			for (PipeSkin skin : PipeSkin.MAP.values())
-			{
-				AbstractMap.SimpleEntry<String, ResourceLocation> entry = new AbstractMap.SimpleEntry<>("material", skin.texture);
-				List<List<BakedQuad>> connection1 = new ArrayList<>(6);
-
-				for (int i = 0; i < 6; i++)
-				{
-					connection1.add(c.get(m.modelConnection, FACE_ROTATIONS[i], entry));
-				}
-
-				connection.put(skin, connection1);
-			}
-
-			for (int i = 0; i < 6; i++)
-			{
-				glassConnection.add(c.get(m.modelGlassConnection, FACE_ROTATIONS[i]));
-			}
-
-			overlay = new ArrayList<>(EnumMK.VALUES.length);
-
-			for (int i = 0; i < EnumMK.VALUES.length; i++)
-			{
-				overlay.add(c.get(m.modelOverlay, ModelRotation.X0_Y0, new AbstractMap.SimpleEntry<>("overlay", m.overlayTextures[i])));
-			}
-
-			cache = new HashMap<>();
-			bakedItem = new BakedItem(null);
-			bakedItemWithOverlay = new BakedItem[EnumMK.VALUES.length];
-
-			for (EnumMK mk : EnumMK.VALUES)
-			{
-				bakedItemWithOverlay[mk.ordinal()] = new BakedItem(mk);
-			}
-
-			itemOverrideList = new ItemOverrideList(Collections.emptyList())
-			{
-				@Override
-				public IBakedModel handleItemState(IBakedModel originalModel, ItemStack stack, @Nullable World world, @Nullable EntityLivingBase entity)
-				{
-					Block block = Block.getBlockFromItem(stack.getItem());
-					return block instanceof BlockPipeBase ? block instanceof BlockPipeModular ? bakedItemWithOverlay[((BlockPipeModular) block).tier.ordinal()] : bakedItem : originalModel;
-				}
-			};
-		}
-
-		@Override
-		public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand)
-		{
-			if (state == null || side != null)
-			{
-				return Collections.emptyList();
-			}
-
-			TilePipeBase pipe = null;
-
-			if (state instanceof IExtendedBlockState)
-			{
-				pipe = ((IExtendedBlockState) state).getValue(BlockPipeBase.PIPE);
-			}
-
-			if (pipe == null || pipe.invisible)
-			{
-				return Collections.emptyList();
-			}
-
-			BlockRenderLayer layer = MinecraftForgeClient.getRenderLayer();
-
-			if (layer == BlockRenderLayer.TRANSLUCENT)
-			{
-				if (pipe instanceof TilePipeModularMK1)
-				{
-					return overlay.get(((TilePipeModularMK1) pipe).getMK().ordinal());
-				}
-				else
-				{
-					return Collections.emptyList();
-				}
-			}
-
-			int connections = 0;
-
-			for (EnumFacing facing : EnumFacing.VALUES)
-			{
-				if (pipe.isConnected(facing))
-				{
-					connections |= 1 << facing.getIndex();
-				}
-			}
-
-			int baseIndex = 0;
-
-			switch (connections)
-			{
-				case 0b110000:
-					baseIndex = 1;
-					break;
-				case 0b000011:
-					baseIndex = 2;
-					break;
-				case 0b001100:
-					baseIndex = 3;
-					break;
-			}
-
-			Int2ObjectOpenHashMap<List<BakedQuad>> cacheMap = cache.get(pipe.skin);
-
-			if (cacheMap == null)
-			{
-				cacheMap = new Int2ObjectOpenHashMap<>();
-				cache.put(pipe.skin, cacheMap);
-			}
-
-			int cacheIndex = connections | ((layer == BlockRenderLayer.CUTOUT ? 1 : 0) << 6);
-
-			List<BakedQuad> quads = cacheMap.get(cacheIndex);
-
-			if (quads != null)
-			{
-				return quads;
-			}
-
-			quads = new ArrayList<>();
-
-			if (layer == BlockRenderLayer.SOLID)
-			{
-				quads.addAll(base.get(pipe.skin).get(baseIndex));
-
-				if (baseIndex == 0)
-				{
-					for (int i = 0; i < 6; i++)
-					{
-						if ((connections & (1 << i)) != 0)
-						{
-							quads.addAll(connection.get(pipe.skin).get(i));
-						}
-					}
-				}
-			}
-
-			if (layer == BlockRenderLayer.CUTOUT)
-			{
-				quads.addAll(glassBase.get(baseIndex));
-
-				if (baseIndex == 0)
-				{
-					for (int i = 0; i < 6; i++)
-					{
-						if ((connections & (1 << i)) != 0)
-						{
-							quads.addAll(glassConnection.get(i));
-						}
-					}
-				}
-			}
-
-			quads = Collections.unmodifiableList(Arrays.asList(quads.toArray(new BakedQuad[0])));
-			cacheMap.put(cacheIndex, quads);
-			return quads;
-		}
-
-		@Override
-		public boolean isAmbientOcclusion()
-		{
-			return true;
-		}
-
-		@Override
-		public boolean isGui3d()
-		{
-			return true;
-		}
-
-		@Override
-		public boolean isBuiltInRenderer()
-		{
-			return false;
-		}
-
-		@Override
-		public TextureAtlasSprite getParticleTexture()
-		{
-			return particle;
-		}
-
-		@Override
-		public ItemOverrideList getOverrides()
-		{
-			return itemOverrideList;
-		}
-
-		@Override
-		public org.apache.commons.lang3.tuple.Pair<? extends IBakedModel, javax.vecmath.Matrix4f> handlePerspective(ItemCameraTransforms.TransformType cameraTransformType)
-		{
-			return handleModelPerspective(this, cameraTransformType);
-		}
 	}
 }
